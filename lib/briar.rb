0c49c2ee8582d489b6626d97c94c5eb7d21474b0
Michael Kirk <michael.john.kirk@gmail.com>
2013-03-10 12:53:04 -0700

    just require briar which already requires all of it's components

--- lib/briar.rb
+++ lib/briar.rb
@@ -31,34 +31,6 @@ require 'briar/table'
 require 'briar/text_field'
 require 'briar/text_view'
 
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions', 'briar_core_steps')
-#
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','alerts_and_sheets', 'action_sheet_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','alerts_and_sheets', 'alert_view_steps')
-#
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','bars', 'tabbar_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','bars', 'navbar_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','bars', 'toolbar_steps')
-#
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','control', 'button_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','control', 'segmented_control_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','control', 'slider_steps')
-#
-#
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','picker', 'picker_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','picker', 'date_picker_steps')
-#
-#
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','email_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','image_view_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','keyboard_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','label_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','scroll_view_steps')
-#
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','table_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','text_field_steps')
-#require File.join(File.dirname(__FILE__), '..','features','step_definitions','text_view_steps')
-
 def gestalt ()
   uri = URI("#{DEVICE_ENDPOINT}/version")
   res = Net::HTTP.get(uri)
