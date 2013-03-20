597b00668ea1e21ca5424b30d036531a99496169
Joshua Moody <joshuajmoody@gmail.com>
2013-03-20 10:20:44 +0100

    should see/should not see step no longer calls should see after animation

--- features/step_definitions/briar_core_steps.rb
+++ features/step_definitions/briar_core_steps.rb
@@ -1,9 +1,9 @@
 #include Briar::Core
 
 Then /^I should (see|not see) (?:the|) "([^\"]*)" view$/ do |visibility, view_id|
-  if visibility.eql? "see"
-    should_see_view_after_animation view_id
+  if visibility.eql? 'see'
+    should_see_view view_id
   else
-    should_not_see_view_after_animation view_id
+    should_not_see_view view_id
   end
 end
