f13cb61681a6dc72e4b57a2d734fa00be4a80b77
Joshua Moody <joshuajmoody@gmail.com>
2013-09-24 13:03:06 +0200

    refactored briar binary commands

--- bin/briar_helpers.rb
+++ bin/briar_helpers.rb
@@ -1,11 +1,13 @@
 require 'briar'
 
 BRIAR_RM_CAL_TARGETS='rm-cal-targets'
-BRIAR_INSTALL_CALABASH_GEM='install-calabash-gem'
-BRIAR_INSTALL_CALABASH_SERVER='install-calabash-server'
+BRIAR_INSTALL_CALABASH_GEM='calabash-gem'
+BRIAR_INSTALL_CALABASH_SERVER='calabash-server'
 BRIAR_INSTALL_GEM='gem'
 BRIAR_RESIGN_IPA='resign'
 BRIAR_VERSION_CMD='version'
+
+BRIAR_CMD_INSTALL='install'
 
 
 def print_usage
@@ -23,16 +25,19 @@ def print_usage
     targets and deletes the enclosing directory.  useful for clearing out old
     calabash targets when the framework needs to be updated.
 
-  briar #{BRIAR_INSTALL_GEM}
+  briar #{BRIAR_CMD_INSTALL} { gem | calabash-gem | calabash-server }
+    subcommand to perform various installation tasks
+
+  briar #{BRIAR_CMD_INSTALL} #{BRIAR_INSTALL_GEM}
     looks for ~/git/briar and runs 'rake install'. useful for developing and
     testing the briar gem
 
-  briar #{BRIAR_INSTALL_CALABASH_GEM}
+  briar #{BRIAR_CMD_INSTALL} #{BRIAR_INSTALL_CALABASH_GEM}
     looks for ~/git/calabash-ios/calabash-cucumber directory and runs
     'rake install' to install the calabash-cucumber gem.  useful for developing
     and testing the calabash-cucumber gem.
 
-  briar #{BRIAR_INSTALL_CALABASH_SERVER}
+  briar #{BRIAR_CMD_INSTALL} #{BRIAR_INSTALL_CALABASH_SERVER}
     WARN: this is a destructive operation!
           * replaces calabash.framework in current directory
           * deletes all *-cal targets from simulator
