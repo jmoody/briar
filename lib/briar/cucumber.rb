9ba7e51dc49bfc289a60b35768f2cea4574f77ee
Joshua Moody <joshuajmoody@gmail.com>
2014-01-17 20:24:56 +0100

    disabling predefined steps on XTC
    see the xamarin-build.sh in briar-ios-example for strategies for
    dealing with this

--- lib/briar/cucumber.rb
+++ lib/briar/cucumber.rb
@@ -28,29 +28,41 @@ World(Briar::Table)
 World(Briar::TextField)
 World(Briar::TextView)
 
+# briar predefined steps are not found during the XTC validation phase
+#
+# they are, however, correctly exposed because if you copy them to the
+# step_definitions directory as part of the XTC submission, you will see
+# Cucumber:Ambiguous errors
+#
+# so the strategy to is avoid loading the step definitions when running on the
+# XTC and copying them to the features/step_definitions directory during the
+# XTC job submission.
+#
+# see the xamarin-build.sh script
+unless ENV['XAMARIN_TEST_CLOUD'] == '1'
+  require_relative '../../features/step_definitions/alerts_and_sheets/action_sheet_steps'
+  require_relative '../../features/step_definitions/alerts_and_sheets/alert_view_steps'
 
-require_relative '../../features/step_definitions/alerts_and_sheets/action_sheet_steps'
-require_relative '../../features/step_definitions/alerts_and_sheets/alert_view_steps'
+  require_relative '../../features/step_definitions/bars/tabbar_steps'
+  require_relative '../../features/step_definitions/bars/navbar_steps'
+  require_relative '../../features/step_definitions/bars/toolbar_steps'
 
-require_relative '../../features/step_definitions/bars/tabbar_steps'
-require_relative '../../features/step_definitions/bars/navbar_steps'
-require_relative '../../features/step_definitions/bars/toolbar_steps'
-
-require_relative '../../features/step_definitions/control/button_steps'
-require_relative '../../features/step_definitions/control/segmented_control_steps'
-require_relative '../../features/step_definitions/control/slider_steps'
+  require_relative '../../features/step_definitions/control/button_steps'
+  require_relative '../../features/step_definitions/control/segmented_control_steps'
+  require_relative '../../features/step_definitions/control/slider_steps'
 
 
-require_relative '../../features/step_definitions/picker/picker_steps'
-require_relative '../../features/step_definitions/picker/date_picker_steps'
+  require_relative '../../features/step_definitions/picker/picker_steps'
+  require_relative '../../features/step_definitions/picker/date_picker_steps'
 
 
-require_relative '../../features/step_definitions/email_steps'
-require_relative '../../features/step_definitions/image_view_steps'
-require_relative '../../features/step_definitions/keyboard_steps'
-require_relative '../../features/step_definitions/label_steps'
-require_relative '../../features/step_definitions/scroll_view_steps'
+  require_relative '../../features/step_definitions/email_steps'
+  require_relative '../../features/step_definitions/image_view_steps'
+  require_relative '../../features/step_definitions/keyboard_steps'
+  require_relative '../../features/step_definitions/label_steps'
+  require_relative '../../features/step_definitions/scroll_view_steps'
 
-require_relative '../../features/step_definitions/table_steps'
-require_relative '../../features/step_definitions/text_field_steps'
-require_relative '../../features/step_definitions/text_view_steps'
+  require_relative '../../features/step_definitions/table_steps'
+  require_relative '../../features/step_definitions/text_field_steps'
+  require_relative '../../features/step_definitions/text_view_steps'
+end
