9dbca5cb60986fb1bb9f8399b7340cfc69e2ee8a
Joshua Moody <joshuajmoody@gmail.com>
2014-04-09 18:05:17 +0200

    should not use 'DEVELOPER_DIR' env var because behavior is unpredictable

--- bin/briar_sim.rb
+++ bin/briar_sim.rb
@@ -24,16 +24,21 @@ def default_simulated_device
   raise "could not find '#{res}' in hash values '#{simulator_hash()}'"
 end
 
+# returns the xcode developer dir using xcode-select
+def xcode_developer_dir
+  `xcode-select --print-path`.chomp
+end
+
 # kills the simulator if it is running
 #
 # uses Apple Script
 def kill_simulator
-  dev_dir = ENV['DEVELOPER_DIR']
+  dev_dir = xcode_developer_dir
   system "/usr/bin/osascript -e 'tell application \"#{dev_dir}/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone Simulator.app\" to quit'"
 end
 
 def open_simulator
-  dev_dir = ENV['DEVELOPER_DIR']
+  dev_dir = xcode_developer_dir
   system "/usr/bin/osascript -e 'tell application \"#{dev_dir}/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone Simulator.app\" to activate'"
 end
 
