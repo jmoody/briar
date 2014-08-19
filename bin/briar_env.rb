d575bc406b9b0b96997411408abc3e47b886da0d
Joshua Moody <joshuajmoody@gmail.com>
2014-08-20 01:24:54 +0200

    should check for nil arg before File.expand(nil)

--- bin/briar_env.rb
+++ bin/briar_env.rb
@@ -12,6 +12,11 @@ def expect_bundle_id
 end
 
 def expect_build_script(build_script)
+  if build_script.nil?
+    @log.fatal{ 'action requires a build script to create an ipa, but IPA_BUILD_SCRIPT has not been set' }
+    @log.fatal{ 'add IPA_BUILD_SCRIPT to your .env file or export it to your shell' }
+    exit 1
+  end
   unless File.exists?(File.expand_path(build_script))
     @log.fatal{ 'arguments say there is supposed to be a build script' }
     @log.fatal{ "expected it here '#{build_script}'" }
@@ -22,6 +27,11 @@ end
 
 def expect_ideviceinstaller
   bin_path = ENV['IDEVICEINSTALLER_BIN']
+  if bin_path.nil?
+    @log.fatal{ 'action requires ideviceinstaller, but IDEVICEINSTALLER_BIN has not been set' }
+    @log.fatal{ 'add IDEVICEINSTALLER_BIN to your .env file or export it to your shell' }
+    exit 1
+  end
   unless File.exists?(File.expand_path(bin_path))
     @log.fatal{'cannot find ideviceinstaller'}
     @log.fatal{ "expected it here '#{bin_path}'"}
@@ -31,6 +41,11 @@ def expect_ideviceinstaller
 end
 
 def expect_ipa(ipa_path)
+  if ipa_path.nil?
+    @log.fatal { 'action requires an ipa, but the IPA variable has not been set' }
+    @log.fatal { 'add IPA to your .env file or export it to your shell' }
+    exit 1
+  end
   unless File.exists?(File.expand_path(ipa_path))
     @log.fatal{'ipa does not exist'}
     @log.fatal{"expected it here '#{ipa_path}'"}
